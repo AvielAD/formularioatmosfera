9679ca017ff0b4a025fdcfd4601cd6d2d85dae44
Aviel Aldama <aviel.aldama@gmail.com>
2023-11-22 22:43:29 -0600

    remove cors module

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 const app = express()
-app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.use('/formularios', Formularios)
