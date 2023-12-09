23f14fa8100b5cb62c402a55dfe35c1ea97507f6
Aviel Aldama <aviel.aldama@gmail.com>
2023-12-08 23:29:32 -0600

    count inscritos by curso

--- prisma/views/public/eventosview.sql
+++ prisma/views/public/eventosview.sql
@@ -7,12 +7,23 @@ SELECT
   e.fechafinpromocion AS finpromocion,
   e.fechainicio AS inicio,
   e2.nombre AS estado,
-  e.uuidpage AS uuid
+  e.uuidpage AS uuid,
+  (count(ea.ideventocurso)) :: integer AS inscritos
 FROM
   (
     (
-      eventocurso e
-      JOIN curso c ON ((e.idcurso = c.id))
+      (
+        eventocurso e
+        JOIN curso c ON ((e.idcurso = c.id))
+      )
+      JOIN estadoeventocurso e2 ON ((e.idestadoeventocurso = e2.id))
     )
-    JOIN estadoeventocurso e2 ON ((e.idestadoeventocurso = e2.id))
-  );
+    LEFT JOIN eventocurso_alumno ea ON ((ea.ideventocurso = e.id))
+  )
+GROUP BY
+  ea.ideventocurso,
+  e.id,
+  c.nombre,
+  c.temario,
+  c.costo,
+  e2.nombre;
